a603815fc756cb850024de4fcf852b73559e3b26
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-02-25 13:53:21 +0100

    added basic html structure

---
+++ view/footer.php
@@ -0,0 +1,10 @@
+<?php
+
+
+?>
+
+<footer class="blog-footer container d-flex justify-content-center mt-5">
+    <div class="text-center">
+        Copyright MarteDL 2021
+    </div>
+</footer>
---
+++ view/header.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<header>
+    <div class="container ">
+        <nav class="navbar navbar-light bg-warning mb-3 rounded">
+            <a class="navbar-brand">Posts</a>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
+        <div class="jumbotron">
+            <h1 class="display-4">My Guestbook</h1>
+            <p class="lead">On this page you can leave a message for me. Kind words, compliments and jokes will gain you karma-points. Profanity, meanness and other shenanigans will get you a nice warm spot in hell.</p>
+            <hr class="my-4">
+            <p>If you can't say anything nice, shut up.</p>
+            <a class="btn btn-warning btn-lg" href="#" role="button">Write message</a>
+        </div>
+    </div>
+</header>
